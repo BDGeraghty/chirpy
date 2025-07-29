6289c0967e6075099047f93ce5659e889327d014
bdgeraghty <bdgeraghty@gmail.com>
2025-07-28 21:32:11 -0400

    Working Chirpy to Stateful Handlers

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
+
+
 import (
-	"log"
-	"net/http"
+   "fmt"
+   "log"
+   "net/http"
+   "sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(
+			http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	//mux.Handle("/app/", cfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("/metrics", cfg.handlerHits)
+	mux.HandleFunc("/reset", cfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -22,9 +38,16 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-	w.Write([]byte("OK"))
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerHits(w http.ResponseWriter, r *http.Request) {
+   w.Header().Add("Content-Type", "application/json; charset=utf-8")
+   w.WriteHeader(http.StatusOK)
+   w.Write([]byte(`{` + fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load()) + `}`))
 }
